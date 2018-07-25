f48dac32cbe7de1e7a5a517252249e2b1c91ac6b
dennisgebken <34404635+dennisgebken@users.noreply.github.com>
2018-07-25 17:10:02 -0400

    Created/Edited 2 new views.
    Add Task: as the name says.
    View task: view only. should be called when a task is clicked in the day-view.
    
    Layout is basic to start working on functionality.

---
+++ Mobile App/Task Manager/app/src/main/java/com/classproject/markngn/taskmanager/add_task.java
@@ -0,0 +1,13 @@
+package com.classproject.markngn.taskmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class add_task extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+    }
+}
